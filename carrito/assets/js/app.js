4520aff3208393ef96e38e365fe9d891c1c9c60f
luis20168 <martinesj405@gmail.com>
2023-08-11 12:02:08 -0500

    cambios en js

--- carrito/assets/js/app.js
+++ carrito/assets/js/app.js
@@ -21,35 +21,70 @@ carrito.addEventListener('mouseover',()=>{
 
 //funcionalidads
 
-const agregado= document.getElementById('agregado');
-agregar.addEventListener('click', ()=>{
-    const card= document.createElement('div');
-    card.classList.add('card');
-    const img= document.createElement('img');
-    img.src= '/carrito/assets/img/curso1.jpg';
-    img.classList.add('imagen_')
-    const h1= document.createElement('h4');
-    h1.textContent= "moto";
-    const h2= document.createElement('h4');
-    h2.textContent= '15k';
-    const h3=document.createElement('h4');
-    h3.textContent='1';
+// const agregado= document.getElementById('agregado');
+// agregar.addEventListener('click', ()=>{
+//     const card= document.createElement('div');
+//     card.classList.add('card');
+//     const img= document.createElement('img');
+//     img.src= '/carrito/assets/img/curso1.jpg';
+//     img.classList.add('imagen_')
+//     const h1= document.createElement('h4');
+//     h1.textContent= "moto";
+//     const h2= document.createElement('h4');
+//     h2.textContent= '15k';
+//     const h3=document.createElement('h4');
+//     h3.textContent='1';
 
-    card.appendChild(img);
-    card.appendChild(h1);
-    card.appendChild(h2);
-    card.appendChild(h3);
-    console.log(card)
-    agregado.appendChild(card)
+//     card.appendChild(img);
+//     card.appendChild(h1);
+//     card.appendChild(h2);
+//     card.appendChild(h3);
+//     console.log(card)
+//     agregado.appendChild(card)
 
+
+// });
+
+
+// const eliminar= document.getElementById('eliminar');
+// eliminar.addEventListener('click', ()=>{
+//     agregado.replaceChildren();
+// });
+
+
+//g---------------------------
+
+const contenidoProductos= document.getElementById('contenido-productos');
+
+let allProducts=[];
+
+
+
+contenidoProductos.addEventListener('click', (e)=>{
+    console.log(e.target.classList.contains('agregar'))
+
+    if(e.target.classList.contains('agregar')){
+        console.log(e.target.parentElement); //con el parent element lo que se hace es llegar al div pafre de el parametro e botton
+        const producto= e.target.parentElement;
+        console.log(producto.querySelector('h4').textContent ) //asi se saca la informacion de cada tarjeta
+        console.log(producto.querySelector('p').textContent)
+
+        const infoProducto= {
+            cantidad: 1,
+            titulo: producto.querySelector('h4').textContent,
+            descripcion: producto.querySelector('p').textContent,
+            precio: producto.querySelector('h5').textContent  
+        }
+
+        console.log(infoProducto)
+
+        let allProducts=[...allProducts, infoProducto];
+
+    }
 
 });
 
-
-const eliminar= document.getElementById('eliminar');
-eliminar.addEventListener('click', ()=>{
-    agregado.replaceChildren();
-});
+    
 
 
 
